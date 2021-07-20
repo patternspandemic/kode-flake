328054eb97a08c73cfdaee9bc7e3f421b97f54bf
patternspandemic <patternspandemic@live.com>
2021-07-20 14:40:52 -0700

    Update input. Fix libKinc build.

--- kinc/default.nix
+++ kinc/default.nix
@@ -13,9 +13,7 @@ in
     installPhase = ''
       mkdir -p $out
       cp -r ./* $out
-# FIXME: graphics2 removal temporary
-      rm $out/Sources/kinc/graphics2/*
-    '';
+   '';
 
     postFixup = lib.optionalString (stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux") ''
       # Patch Binaries
